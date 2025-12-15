0ff5f99cc2fa1d676d7b8101f6b2d825730cd349
Juanse <j.s.calvinio@gmail.com>
2025-12-14 23:56:21 -0300

    Agregamos nuevo diseño de shadcn, login y register

--- src/core/modules/users/application/use-cases/LoginUser.ts
+++ src/core/modules/users/application/use-cases/LoginUser.ts
@@ -1,11 +1,8 @@
-import type {
-  LoginRequestDTO,
-  LoginResponseDTO,
-} from "../../domain/models/Login";
+import type { AuthResponse, LoginRequest } from "../../domain/models/Auth";
 import type { UsersRepository } from "../../domain/repositories/UsersRepository";
 
 export function LoginUser(userRepository: UsersRepository) {
-  return async function (user: LoginRequestDTO): Promise<LoginResponseDTO> {
+  return async function (user: LoginRequest): Promise<AuthResponse> {
     return await userRepository.login(user);
   };
 }
